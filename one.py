8811b4dc2e1d94b6ec0989a8e6932b595fb679fa
PurpleZeroys <nenilin.vladislav.101@gmail.com>
2020-12-14 19:50:27 +0500

    Программа рисующая смайлик..

--- one.py
+++ one.py
@@ -1,20 +1,31 @@
-from random import randint
-import turtle
+import pygame
+from pygame.draw import *
 
+pygame.init()
 
-number_of_turtles = 5
-steps_of_time_number = 1000
+FPS = 30
+screen = pygame.display.set_mode((400, 400))
 
+screen.fill('white')
+circle(screen, (255,190,70), (200, 200), 70)
+circle(screen, (1,1,1), (200, 200), 70, 1)
+circle(screen, ('white'), (170, 170), 20)
+circle(screen, ('white'), (230, 170), 20)
+circle(screen, 'black', (170, 170), 20, 1)
+circle(screen, ('black'), (230, 170), 20, 1)
+circle(screen, ('black'), (170, 170), 10)
+circle(screen, ('black'), (230, 170), 10)
+polygon(screen, (0, 0, 0), [(170,220), (230,220),
+                               (220,230), (180,230) ])
 
-pool = [turtle.Turtle(shape='turtle') for i in range(number_of_turtles)]
-for unit in pool:
-    unit.penup()
-    unit.speed(500)
-    unit.goto(randint(-200, 200), randint(-200, 200))
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
 
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
 
-for i in range(steps_of_time_number):
-    for unit in pool:
-        unit.forward(2)
-
-turtle.done()
+pygame.quit()
